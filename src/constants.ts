e3b6f7a184368b279c580827e5d4ba383fd8118d
sanogosy <sanogoi30@yahoo.fr>
2026-07-11 19:49:24 +0200

    update URL in constants.ts for production

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,11 @@
-export const baseUrl = "http://localhost:8083/api/v1";
+// production
+export const baseUrl = "http://100.31.199.36:8083/api/v1";
 export const chatUrl = baseUrl + "/chat/question";
-export const baseUrlDocument = "http://localhost:8082/api/v1";
+export const baseUrlDocument = "http://100.31.199.36:8082/api/v1";
 export const apiUrlDocument = baseUrlDocument + "/documents/upload";
+
+// local
+// export const baseUrl = "http://localhost:8083/api/v1";
+// export const chatUrl = baseUrl + "/chat/question";
+// export const baseUrlDocument = "http://localhost:8082/api/v1";
+// export const apiUrlDocument = baseUrlDocument + "/documents/upload";
